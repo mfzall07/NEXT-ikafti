82a15e552b8beb0afb6bf16bdec55a93c62dd167
fzall <mfaisalhafid@gmail.com>
2022-08-21 20:09:56 +0700

    fixed login

--- resources/views/livewire/modal/login.blade.php
+++ resources/views/livewire/modal/login.blade.php
@@ -18,15 +18,6 @@
                         <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Your password</label>
                         <input type="password" wire:model.defer="password" name="password" id="password" placeholder="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required>
                     </div>
-                    <div class="flex justify-between">
-                        <div class="flex items-start">
-                            <div class="flex items-center h-5">
-                                <input id="remember" type="checkbox" value="" class="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300">
-                            </div>
-                            <label for="remember" class="ml-2 text-sm font-medium text-gray-900">Remember me</label>
-                        </div>
-                        <a href="#" class="text-sm text-blue-700 hover:underline">Lost Password?</a>
-                    </div>
                     <button type="submit" wire:click.prevent="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Login to your account</button>
                 </form>
             </div>
