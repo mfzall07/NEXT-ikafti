6d198b1b7d92b07a98960f67f80d5dcb8e45f197
Habib Ali Habsyi <habibalihabsyi123@gmail.com>
2022-08-23 04:08:28 +0700

    Page Alumni List

--- app/Http/Livewire/Card/CardAlumni.php
+++ app/Http/Livewire/Card/CardAlumni.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Livewire\Card;
 
+use App\Models\Alumni;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class CardAlumni extends Component
 {
+    use WithPagination;
     public function render()
     {
-        return view('livewire.card.card-alumni');
+        $alumnis = Alumni::doesntHave('waiting_list')->latest()->paginate(8, ['*'], 'alumniPage');
+        return view('livewire.card.card-alumni', compact('alumnis'));
     }
 }
--- resources/views/livewire/card/card-alumni.blade.php
+++ resources/views/livewire/card/card-alumni.blade.php
@@ -14,7 +14,52 @@
         </div>
     </div>
     <div class="flex flex-col laptop:grid grid-cols-12 items-center justify-between flex-wrap gap-5" data-aos="fade-left" data-aos-anchor-placement="top-bottom" data-aos-duration="2000">
+        @forelse ($alumnis as $alumni)
         <div class="bg-white rounded-xl border shadow-lg relative overflow-hidden w-full col-span-3">
+            <div class="h-24 w-24 bg-red-300 absolute -bottom-12 -right-12 rounded-tr-full rounded-br-full rounded-bl-full z-20"></div>
+            <div class="h-24 w-24 bg-red-400 absolute -bottom-10 -right-10 rounded-tr-full rounded-br-full rounded-bl-full z-10"></div>
+            <div class="flex flex-col">
+                <div class="rounded-lg shadow-xl">
+                    <div style="background-image: url({{ url( $alumni->image != null ? str_replace('public', 'storage',$alumni->image) : 'assets/images/profile.jpg')}})" class="bg-cover bg-center w-full h-36 flex items-center justify-center">
+                        <img src="{{ url( $alumni->image != null ? str_replace('public', 'storage',$alumni->image) : 'assets/images/profile.jpg')}}" class="w-28 h-28 rounded-full object-cover border-2 border-white shadow-xl" alt="">
+                    </div>
+                </div>
+                <div class="flex flex-col gap-5 p-5">
+                    <div class="grid grid-cols-12 gap-5">
+                        <i class="col-span-2 mx-auto fa-solid fa-user-tie"></i>
+                        <h1 class="col-span-10 text-sm font-semibold text-gray-400">{{ $alumni->name ?? '-' }}</h1>
+                    </div>
+                    <div class="grid grid-cols-12 gap-5">
+                        <i class="col-span-2 mx-auto fa-solid fa-map-location-dot"></i>
+                        <h1 class="col-span-10 text-sm font-semibold text-gray-400">{{ $alumni->address ?? '-' }}</h1>
+                    </div>
+                    <div class="grid grid-cols-12 gap-5">
+                        <i class="col-span-2 mx-auto fa-solid fa-house-circle-check"></i>
+                        <h1 class="col-span-10 text-sm font-semibold text-gray-400">{{ $alumni->domicile ?? '-' }}</h1>
+                    </div>
+                    <div class="grid grid-cols-12 gap-5">
+                        <i class="col-span-2 mx-auto fa-solid fa-envelope"></i>
+                        <h1 class="col-span-10 text-sm font-semibold text-gray-400">{{ $alumni->email ?? '-' }}</h1>
+                    </div>
+                    <div class="grid grid-cols-12 gap-5">
+                        <i class="col-span-2 mx-auto fa-solid fa-square-phone"></i>
+                        <h1 class="col-span-10 text-sm font-semibold text-gray-400">{{ $alumni->phone ?? '-' }}</h1>
+                    </div>
+                    <div class="grid grid-cols-12 gap-5">
+                        <i class="col-span-2 mx-auto fa-solid fa-users"></i>
+                        <h1 class="col-span-10 text-sm font-semibold text-gray-400">{{ $alumni->generation ?? '-' }}</h1>
+                    </div>
+                    <div class="grid grid-cols-12 gap-5">
+                        <i class="col-span-2 mx-auto fa-solid fa-building"></i>
+                        <h1 class="col-span-10 text-sm font-semibold text-gray-400">{{ $alumni->company ?? '-' }}</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        @empty
+
+        @endforelse
+        {{-- <div class="bg-white rounded-xl border shadow-lg relative overflow-hidden w-full col-span-3">
             <div class="h-24 w-24 bg-red-300 absolute -bottom-12 -right-12 rounded-tr-full rounded-br-full rounded-bl-full z-20"></div>
             <div class="h-24 w-24 bg-red-400 absolute -bottom-10 -right-10 rounded-tr-full rounded-br-full rounded-bl-full z-10"></div>
             <div class="flex flex-col">
@@ -25,27 +70,27 @@
                 </div>
                 <div class="flex flex-col gap-5 p-5">
                     <div class="grid grid-cols-12 gap-5">
-                        <i class="col-span-2 mx-auto fa-solid fa-user-tie"></i> 
+                        <i class="col-span-2 mx-auto fa-solid fa-user-tie"></i>
                         <h1 class="col-span-10 text-sm font-semibold text-gray-400">Muh Faizal</h1>
                     </div>
                     <div class="grid grid-cols-12 gap-5">
-                        <i class="col-span-2 mx-auto fa-solid fa-map-location-dot"></i>   
+                        <i class="col-span-2 mx-auto fa-solid fa-map-location-dot"></i>
                         <h1 class="col-span-10 text-sm font-semibold text-gray-400">Jl. Beringin</h1>
                     </div>
                     <div class="grid grid-cols-12 gap-5">
-                        <i class="col-span-2 mx-auto fa-solid fa-house-circle-check"></i>   
+                        <i class="col-span-2 mx-auto fa-solid fa-house-circle-check"></i>
                         <h1 class="col-span-10 text-sm font-semibold text-gray-400">Palu</h1>
                     </div>
                     <div class="grid grid-cols-12 gap-5">
-                        <i class="col-span-2 mx-auto fa-solid fa-envelope"></i>  
+                        <i class="col-span-2 mx-auto fa-solid fa-envelope"></i>
                         <h1 class="col-span-10 text-sm font-semibold text-gray-400">faizal@gmail.com</h1>
                     </div>
                     <div class="grid grid-cols-12 gap-5">
-                        <i class="col-span-2 mx-auto fa-solid fa-square-phone"></i>  
+                        <i class="col-span-2 mx-auto fa-solid fa-square-phone"></i>
                         <h1 class="col-span-10 text-sm font-semibold text-gray-400">+6282xxxxxx</h1>
                     </div>
                     <div class="grid grid-cols-12 gap-5">
-                        <i class="col-span-2 mx-auto fa-solid fa-users"></i>  
+                        <i class="col-span-2 mx-auto fa-solid fa-users"></i>
                         <h1 class="col-span-10 text-sm font-semibold text-gray-400">2017</h1>
                     </div>
                     <div class="grid grid-cols-12 gap-5">
@@ -66,27 +111,27 @@
                 </div>
                 <div class="flex flex-col gap-5 p-5">
                     <div class="grid grid-cols-12 gap-5">
-                        <i class="col-span-2 mx-auto fa-solid fa-user-tie"></i> 
+                        <i class="col-span-2 mx-auto fa-solid fa-user-tie"></i>
                         <h1 class="col-span-10 text-sm font-semibold text-gray-400">Muh Faizal</h1>
                     </div>
                     <div class="grid grid-cols-12 gap-5">
-                        <i class="col-span-2 mx-auto fa-solid fa-map-location-dot"></i>   
+                        <i class="col-span-2 mx-auto fa-solid fa-map-location-dot"></i>
                         <h1 class="col-span-10 text-sm font-semibold text-gray-400">Jl. Beringin</h1>
                     </div>
                     <div class="grid grid-cols-12 gap-5">
-                        <i class="col-span-2 mx-auto fa-solid fa-house-circle-check"></i>   
+                        <i class="col-span-2 mx-auto fa-solid fa-house-circle-check"></i>
                         <h1 class="col-span-10 text-sm font-semibold text-gray-400">Palu</h1>
                     </div>
                     <div class="grid grid-cols-12 gap-5">
-                        <i class="col-span-2 mx-auto fa-solid fa-envelope"></i>  
+                        <i class="col-span-2 mx-auto fa-solid fa-envelope"></i>
                         <h1 class="col-span-10 text-sm font-semibold text-gray-400">faizal@gmail.com</h1>
                     </div>
                     <div class="grid grid-cols-12 gap-5">
-                        <i class="col-span-2 mx-auto fa-solid fa-square-phone"></i>  
+                        <i class="col-span-2 mx-auto fa-solid fa-square-phone"></i>
                         <h1 class="col-span-10 text-sm font-semibold text-gray-400">+6282xxxxxx</h1>
                     </div>
                     <div class="grid grid-cols-12 gap-5">
-                        <i class="col-span-2 mx-auto fa-solid fa-users"></i>  
+                        <i class="col-span-2 mx-auto fa-solid fa-users"></i>
                         <h1 class="col-span-10 text-sm font-semibold text-gray-400">2017</h1>
                     </div>
                     <div class="grid grid-cols-12 gap-5">
@@ -95,50 +140,10 @@
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="bg-white rounded-xl border shadow-lg relative overflow-hidden w-full col-span-3">
-            <div class="h-24 w-24 bg-red-300 absolute -bottom-12 -right-12 rounded-tr-full rounded-br-full rounded-bl-full z-20"></div>
-            <div class="h-24 w-24 bg-red-400 absolute -bottom-10 -right-10 rounded-tr-full rounded-br-full rounded-bl-full z-10"></div>
-            <div class="flex flex-col">
-                <div class="rounded-lg shadow-xl">
-                    <div style="background-image: url('../../assets/images/profile.jpg')" class="bg-cover bg-center w-full h-36 flex items-center justify-center">
-                        <img src="../../assets/images/profile.jpg" class="w-28 h-28 rounded-full object-cover border-2 border-white shadow-xl" alt="">
-                    </div>
-                </div>
-                <div class="flex flex-col gap-5 p-5">
-                    <div class="grid grid-cols-12 gap-5">
-                        <i class="col-span-2 mx-auto fa-solid fa-user-tie"></i> 
-                        <h1 class="col-span-10 text-sm font-semibold text-gray-400">Muh Faizal</h1>
-                    </div>
-                    <div class="grid grid-cols-12 gap-5">
-                        <i class="col-span-2 mx-auto fa-solid fa-map-location-dot"></i>   
-                        <h1 class="col-span-10 text-sm font-semibold text-gray-400">Jl. Beringin</h1>
-                    </div>
-                    <div class="grid grid-cols-12 gap-5">
-                        <i class="col-span-2 mx-auto fa-solid fa-house-circle-check"></i>   
-                        <h1 class="col-span-10 text-sm font-semibold text-gray-400">Palu</h1>
-                    </div>
-                    <div class="grid grid-cols-12 gap-5">
-                        <i class="col-span-2 mx-auto fa-solid fa-envelope"></i>  
-                        <h1 class="col-span-10 text-sm font-semibold text-gray-400">faizal@gmail.com</h1>
-                    </div>
-                    <div class="grid grid-cols-12 gap-5">
-                        <i class="col-span-2 mx-auto fa-solid fa-square-phone"></i>  
-                        <h1 class="col-span-10 text-sm font-semibold text-gray-400">+6282xxxxxx</h1>
-                    </div>
-                    <div class="grid grid-cols-12 gap-5">
-                        <i class="col-span-2 mx-auto fa-solid fa-users"></i>  
-                        <h1 class="col-span-10 text-sm font-semibold text-gray-400">2017</h1>
-                    </div>
-                    <div class="grid grid-cols-12 gap-5">
-                        <i class="col-span-2 mx-auto fa-solid fa-building"></i>
-                        <h1 class="col-span-10 text-sm font-semibold text-gray-400">PT. VIN PROTOCOL</h1>
-                    </div>
-                </div>
-            </div>
-        </div>
+        </div> --}}
     </div>
-    <div class="text-xs text-gray-700 bg-white dark:bg-gray-800 dark:text-gray-400 rounded-b-lg px-6 mt-10 flex flex-col laptop:flex-row items-center justify-between gap-4 ">
+    {{ $alumnis->onEachSide(1)->links('livewire.custom-pagination-links-view', ['pageName' => $alumnis->getPageName()]) }}
+    {{-- <div class="text-xs text-gray-700 bg-white dark:bg-gray-800 dark:text-gray-400 rounded-b-lg px-6 mt-10 flex flex-col laptop:flex-row items-center justify-between gap-4 ">
         <span class="text-sm text-gray-700 dark:text-gray-400">
             Showing <span class="font-semibold text-gray-900 dark:text-white">1</span> to <span class="font-semibold text-gray-900 dark:text-white">8</span> of <span class="font-semibold text-gray-900 dark:text-white">100</span> Entries
         </span>
@@ -173,5 +178,5 @@
                 </li>
             </ul>
         </nav>
-    </div>
+    </div> --}}
 </div>
