47698360d1754aca03e7b5132b92ef95ca20e9ea
fzall <mfaisalhafid@gmail.com>
2022-08-25 23:59:06 +0700

    information right

--- resources/views/livewire/card/informasi-right.blade.php
+++ resources/views/livewire/card/informasi-right.blade.php
@@ -1,13 +1,13 @@
 <div class="flex flex-col gap-4">
     @forelse ($infos as $info)
-    <div class="bg-white rounded-lg shadow-md border p-5 flex items-center gap-4">
+    <div class="bg-white rounded-lg shadow-md border flex items-center gap-4">
+        <div style="background: url('../assets/images/exampleContent.jpg');" class="w-36 h-36 rounded-lg bg-center bg-cover"></div>
         <div class="p-1">
             <div>
                 <a href="{{ route('informationDetail', ['id' => $info->id]) }}" id="title" class="font-bold text-xl hover:text-blue-500 break-words line-clamp-1">{{ $info->title }}</a>
                 {{-- <p id="desc" class="text-gray-400 text-sm break-words mt-4 line-clamp-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p> --}}
             </div>
             <div class="flex items-center gap-4 mt-4">
-                <div style="background: url('../assets/images/exampleContent.jpg')" class="w-8 h-8 rounded-lg bg-center bg-cover"></div>
                 <h1 class="font-bold">{{ $info->author }}</h1>
                 <div class="w-3 h-3 bg-green-400 rounded-full shadow-sm"></div>
                 <p class="text-gray-400">{{ date('d M Y', strtotime($info->created_at)) }}</p>
