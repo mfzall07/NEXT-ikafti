56c758aa2c2e4c961a49ebff2a36b622eccd4b6d
Habib Ali Habsyi <habibalihabsyi123@gmail.com>
2022-08-25 20:14:56 +0700

    Dynamic total content

--- app/Http/Livewire/Card/TotalContent.php
+++ app/Http/Livewire/Card/TotalContent.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Livewire\Card;
 
+use App\Models\Content;
 use Livewire\Component;
 
 class TotalContent extends Component
 {
+    public $content_count;
+    protected $listeners = [
+        'contentDeleted' => '$refresh'
+    ];
     public function render()
     {
+        $this->content_count = Content::count();
         return view('livewire.card.total-content');
     }
 }
