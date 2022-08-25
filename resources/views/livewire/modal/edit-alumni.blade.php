102f15b515388b3fa0f06b36b977761464fc55ed
fzall <mfaisalhafid@gmail.com>
2022-08-25 23:33:31 +0700

    edit alumni

--- resources/views/livewire/modal/edit-alumni.blade.php
+++ resources/views/livewire/modal/edit-alumni.blade.php
@@ -97,35 +97,12 @@
                                 @error('program_studi') <span class="text-red-500">{{ $message }}</span> @enderror
                             </div>
                         </div>
-                        <div>
+                        <div class="w-full flex">
                             @if ($proof)
-                                <a class="mb-2" wire:click="download('{{ str_replace('public/proof/', '', $proof) }}')">{{ str_replace('public/proof/', '', $proof) }}</a>
+                                    <a class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center cursor-pointer" wire:click="download('{{ str_replace('public/proof/', '', $proof) }}')">Download File</a>
                             @endif
-                            <div class="flex justify-center items-center w-full">
-                                <label for="dropzone-files" class="flex flex-col justify-center items-center w-full h-64 bg-gray-50 rounded-lg border-2 border-gray-300 border-dashed cursor-pointer dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-                                    <div class="flex flex-col justify-center items-center pt-5 pb-6">
-                                        <svg aria-hidden="true" class="mb-3 w-10 h-10 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
-                                        @if ($new_proof)
-                                            @if($errors->has('new_proof'))
-                                                <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload the proof</span> or drag and drop</p>
-                                                <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG & PDF</p>
-                                            @else
-                                                <p class="text-xs text-gray-500 dark:text-gray-400"><span class="font-semibold">Your proof has been uploaded</span>
-                                            @endif
-                                        @else
-                                            <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload the proof</span> or drag and drop</p>
-                                            <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG & PDF</p>
-                                        @endif
-                                    </div>
-                                    <input id="dropzone-files" wire:model="new_proof" type="file" accept=".png,.jpg,.jpeg,.pdf" class="hidden" />
-                                    <div wire:loading wire:target="new_proof">
-                                        Uploading...
-                                    </div>
-                                    @error('new_proof') <span class="text-red-500">{{ $message }}</span> @enderror
-                                </label>
-                            </div>
                         </div>
-                        <button type="submit" wire:click.prevent="update" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">edit Alumni</button>
+                        <button type="submit" wire:click.prevent="update" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Edit Alumni</button>
                     </form>
                 </div>
             </div>
