df3b6c247982f12c7500488ef6b816ca701b9322
Habib Ali Habsyi <habibalihabsyi123@gmail.com>
2022-09-04 21:24:41 +0700

    API Store Announcement

--- app/Http/Controllers/API/AnnouncementController.php
+++ app/Http/Controllers/API/AnnouncementController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\Announcement;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AnnouncementController extends Controller
 {
@@ -16,5 +17,27 @@ class AnnouncementController extends Controller
             'message' => 'Data Announcement',
             'data' => $ann
         ],200);
+    }
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'description' => 'required',
+            'image' => 'nullable|image'
+        ]);
+        if($validator->fails()){
+            return response()->json([
+                'success' => false,
+                'message' => 'Validation Error',
+                'errors' => $validator->errors()
+            ]);
+        }
+        $validated = $validator->validate();
+        $ann = Announcement::create($validated);
+        return response()->json([
+            'success' => true,
+            'message' => 'Announcement has been added',
+            'data' => $ann
+        ], 201);
     }
 }
