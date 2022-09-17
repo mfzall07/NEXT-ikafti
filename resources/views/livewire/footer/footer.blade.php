99321eb14909719c46a5cb1e472faa368266ce02
mfzall07 <mfaisalhafid@gmail.com>
2022-09-17 18:39:01 +0700

    update

--- resources/views/livewire/footer/footer.blade.php
+++ resources/views/livewire/footer/footer.blade.php
@@ -1,61 +1,61 @@
 <div class="bg-black shadow px-10 laptop:px-20 py-20">
-    <div class="flex flex-col laptop:flex-row gap-5 items-baseline justify-between">
+    <div class="flex flex-col laptop:flex-row space-y-5 items-baseline justify-between">
         <img src="../../assets/images/logo/IkaftiWhite.png" class="w-56 self-center" alt="">
-        <div class="flex flex-col gap-5 laptop:self-center">
-            <div class="flex items-center gap-4">
+        <div class="flex flex-col space-y-5 laptop:self-center">
+            <div class="flex items-center space-x-4">
                 <i class="fa-solid fa-location-dot text-white"></i>
                 <h1 class="text-white">Jalan Karah Agung I No. 34 Surabaya</h1>
             </div>
-            <div class="flex items-center gap-4">
+            <div class="flex items-center space-x-4">
                 <i class="fa-solid fa-envelope text-white"></i>
                 <h1 class="text-white">admin@ikafti-umi.com</h1>
             </div>
-            <div class="flex items-center gap-4">
+            <div class="flex items-center space-x-4">
                 <i class="fa-solid fa-square-phone text-white"></i>
                 <h1 class="text-white">+62 81239898827</h1>
             </div>
         </div>
-        <div class="flex flex-col gap-5">
+        <div class="flex flex-col space-y-5">
             <h1 class="font-bold text-white">Overview</h1>
-            <div class="flex flex-col gap-5">
-                <a href="/" class="text-gray-300 flex items-center gap-4 hover:text-blue-500 text-sm">
+            <div class="flex flex-col space-y-5">
+                <a href="/" class="text-gray-300 flex items-center space-x-4 hover:text-blue-500 text-sm">
                     <i class="fa-solid fa-house"></i>
-                    Beranda
+                    <span>Beranda</span>
                 </a>
-                <a href="/ikafti/form-register" class="text-gray-300 flex items-center gap-4 hover:text-blue-500 text-sm">
+                <a href="/ikafti/form-register" class="text-gray-300 flex items-center space-x-4 hover:text-blue-500 text-sm">
                     <i class="fa-solid fa-file-lines"></i>
-                    Alumni Registration
+                    <span>Alumni Registration</span>
                 </a>
-                <a href="/ikafti/list-alumni" class="text-gray-300 flex items-center gap-4 hover:text-blue-500 text-sm">
+                <a href="/ikafti/list-alumni" class="text-gray-300 flex items-center space-x-4 hover:text-blue-500 text-sm">
                     <i class="fa-solid fa-sheet-plastic"></i>
-                    Alumni List
+                    <span>Alumni List</span>
                 </a>
-                <a href="/ikafti/tentang-kami" class="text-gray-300 flex items-center gap-4 hover:text-blue-500 text-sm">
+                <a href="/ikafti/tentang-kami" class="text-gray-300 flex items-center space-x-4 hover:text-blue-500 text-sm">
                     <i class="fa-solid fa-user"></i>
-                    About
+                    <span>About</span>
                 </a>
             </div>
         </div>
-        <div class="flex flex-col gap-5">
+        <div class="flex flex-col space-y-5">
             <h1 class="font-bold text-white">Information</h1>
-            <div class="flex flex-col gap-5">
-                <a href="/ikafti/information" class="text-gray-300 flex items-center gap-4 hover:text-blue-500 text-sm">
+            <div class="flex flex-col space-y-5">
+                <a href="/ikafti/information" class="text-gray-300 flex items-center space-x-4 hover:text-blue-500 text-sm">
                     <i class="fa-solid fa-book"></i>
-                    Information
+                    <span>Information</span>
                 </a>
-                <a href="/ikafti/agenda" class="text-gray-300 flex items-center gap-4 hover:text-blue-500 text-sm">
+                <a href="/ikafti/agenda" class="text-gray-300 flex items-center space-x-4 hover:text-blue-500 text-sm">
                     <i class="fa-solid fa-calendar-day"></i>
-                    Agenda
+                    <span>Agenda</span>
                 </a>
-                <a href="/ikafti/karir" class="text-gray-300 flex items-center gap-4 hover:text-blue-500 text-sm">
+                <a href="/ikafti/karir" class="text-gray-300 flex items-center space-x-4 hover:text-blue-500 text-sm">
                     <i class="fa-solid fa-briefcase"></i>
-                    Career
+                    <span>Career</span>
                 </a>
             </div>
         </div>
-        <div class="flex flex-col gap-5">
+        <div class="flex flex-col space-y-5">
             <h1 class="font-bold text-white">Social Media</h1>
-            <div class="flex items-center gap-3">
+            <div class="flex items-center space-x-5">
                 <a href="" class="text-xl h-8 w-8 flex items-center justify-center rounded-full bg-white">
                     <i class="fa-brands fa-instagram"></i>
                 </a>
