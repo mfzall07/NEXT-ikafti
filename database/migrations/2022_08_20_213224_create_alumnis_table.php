70b99322af3d7e107b9883ac43040c2dfba876e1
Habib Ali Habsyi <habibalihabsyi123@gmail.com>
2022-09-17 11:15:32 +0700

    Handle job checkbox

--- database/migrations/2022_08_20_213224_create_alumnis_table.php
+++ database/migrations/2022_08_20_213224_create_alumnis_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('alumnis', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('company');
+            $table->string('company')->nullable();
             $table->text('address');
             $table->string('domicile');
             $table->string('email')->unique();
--- resources/views/livewire/form/register-alumni.blade.php
+++ resources/views/livewire/form/register-alumni.blade.php
@@ -62,14 +62,16 @@
             </div>
             <div class="md:gap-6">
                 <div class="flex items-center gap-4">
-                    <input type="checkbox" name="" id="">
-                    <label for="">List Your Company</label>
+                    <input type="checkbox" name="" id="hasJob" wire:model="hasJob">
+                    <label for="hasJob">List Your Company</label>
                 </div>
-                <div class="relative z-0 mb-6 w-full group">
-                    <input type="text" wire:model.defer="company" name="company" id="company" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                    <label for="company" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Current Company</label>
-                    @error('company') <span class="text-red-500">{{ $message }}</span> @enderror
-                </div>
+                @if ($hasJob)
+                    <div class="relative z-0 mb-6 w-full group">
+                        <input type="text" wire:model.defer="company" name="company" id="company" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                        <label for="company" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Current Company</label>
+                        @error('company') <span class="text-red-500">{{ $message }}</span> @enderror
+                    </div>
+                @endif
             </div>
             <div class="relative z-0 mb-6 w-full group">
                 <div class="flex justify-center items-center w-full">
