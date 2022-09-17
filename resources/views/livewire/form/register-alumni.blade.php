45ab6f0c842b3701538504037e9c10d2df159373
mfzall07 <mfaisalhafid@gmail.com>
2022-09-17 10:54:31 +0700

    push

--- resources/views/livewire/form/register-alumni.blade.php
+++ resources/views/livewire/form/register-alumni.blade.php
@@ -43,7 +43,7 @@
                 @error('birth_date') <span class="text-red-500">{{ $message }}</span> @enderror
               </div>
             </div>
-            <div class="grid md:grid-cols-2 md:gap-6">
+            <div class="grid md:grid-cols-3 md:gap-6">
               <div class="relative z-0 mb-6 w-full group">
                     <input type="number" wire:model.defer="generation" name="angakatan" id="angakatan" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                     <label for="angakatan" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Generation</label>
@@ -54,12 +54,16 @@
                     <label for="domisili" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Domicile</label>
                     @error('domicile') <span class="text-red-500">{{ $message }}</span> @enderror
               </div>
+              <div class="relative z-0 mb-6 w-full group">
+                <input type="tel" wire:model.defer="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="floating_phone" id="floating_phone" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                <label for="floating_phone" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone Number</label>
+                @error('phone') <span class="text-red-500">{{ $message }}</span> @enderror
             </div>
-            <div class="grid md:grid-cols-2 md:gap-6">
-                <div class="relative z-0 mb-6 w-full group">
-                    <input type="tel" wire:model.defer="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="floating_phone" id="floating_phone" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                    <label for="floating_phone" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone Number</label>
-                    @error('phone') <span class="text-red-500">{{ $message }}</span> @enderror
+            </div>
+            <div class="md:gap-6">
+                <div class="flex items-center gap-4">
+                    <input type="checkbox" name="" id="">
+                    <label for="">List Your Company</label>
                 </div>
                 <div class="relative z-0 mb-6 w-full group">
                     <input type="text" wire:model.defer="company" name="company" id="company" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
