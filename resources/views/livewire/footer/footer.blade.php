a2fdd6834127f6a00eff9d932688e3ffc463646c
mfzall07 <mfaisalhafid@gmail.com>
2022-10-04 08:41:57 +0700

    fixed

--- resources/views/livewire/footer/footer.blade.php
+++ resources/views/livewire/footer/footer.blade.php
@@ -3,8 +3,12 @@
         <img src="../../assets/images/logo/IkaftiWhite.png" class="w-56 self-center" alt="">
         <div class="flex flex-col space-y-5 laptop:self-center">
             <div class="flex items-center space-x-4">
-                <i class="fa-solid fa-location-dot text-white"></i>
-                <h1 class="text-white">Jalan Karah Agung I No. 34 Surabaya</h1>
+                <i class="fa-solid fa-location-dot text-white self-baseline mt-1"></i>
+                <div>
+                    <h1 class="text-white">Sekret ika fti umi :</h1>
+                    <h1 class="text-white">Fak Teknologi  Industri Kampus 2 Univ Muslim Indonesia.</h1>
+                    <h1 class="text-white">Jln. Urip Soemoharjo Km 5 Makassar</h1>
+                </div>
             </div>
             <div class="flex items-center space-x-4">
                 <i class="fa-solid fa-envelope text-white"></i>
@@ -54,21 +58,21 @@
             </div>
         </div>
         <div class="flex flex-col space-y-5">
-            <h1 class="font-bold text-white">Social Media</h1>
-            <div class="flex items-center space-x-5">
-                <a href="" class="text-xl h-8 w-8 flex items-center justify-center rounded-full bg-white">
-                    <i class="fa-brands fa-instagram"></i>
-                </a>
-                <a href="" class="text-xl h-8 w-8 flex items-center justify-center rounded-full bg-white">
-                    <i class="fa-brands fa-facebook-f"></i>
-                </a>
-                <a href="" class="text-xl h-8 w-8 flex items-center justify-center rounded-full bg-white">
-                    <i class="fa-brands fa-youtube"></i>
-                </a>
-                <a href="" class="text-xl h-8 w-8 flex items-center justify-center rounded-full bg-white">
-                    <i class="fa-brands fa-linkedin-in"></i>
-                </a>
-            </div>
+            <!--<h1 class="font-bold text-white">Social Media</h1>-->
+            <!--<div class="flex items-center space-x-5">-->
+            <!--    <a href="" class="text-xl h-8 w-8 flex items-center justify-center rounded-full bg-white">-->
+            <!--        <i class="fa-brands fa-instagram"></i>-->
+            <!--    </a>-->
+            <!--    <a href="" class="text-xl h-8 w-8 flex items-center justify-center rounded-full bg-white">-->
+            <!--        <i class="fa-brands fa-facebook-f"></i>-->
+            <!--    </a>-->
+            <!--    <a href="" class="text-xl h-8 w-8 flex items-center justify-center rounded-full bg-white">-->
+            <!--        <i class="fa-brands fa-youtube"></i>-->
+            <!--    </a>-->
+            <!--    <a href="" class="text-xl h-8 w-8 flex items-center justify-center rounded-full bg-white">-->
+            <!--        <i class="fa-brands fa-linkedin-in"></i>-->
+            <!--    </a>-->
+            <!--</div>-->
         </div>
     </div>
 </div>
